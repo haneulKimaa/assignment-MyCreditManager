e1cd2e4f804e6d835ebf5e1d984b80480bece38d
haneulKimaa <khnpoq@naver.com>
2022-11-18 19:35:39 +0900

    [#1] 프로그램 기본 구조 구현

--- MyCreditManager/MyCreditManager/main.swift
+++ MyCreditManager/MyCreditManager/main.swift
@@ -7,3 +7,51 @@
 
 import Foundation
 
+private func getMenu() {
+    print(Description.inputMenu)
+    let readline = readLine()!
+    switch readline {
+    case "1":
+        addStudent(readline)
+    case "2":
+        removeStudent(readline)
+    case "3":
+        removeStudent(readline)
+    case "4":
+        addStudent(readline)
+    case "5":
+        removeStudent(readline)
+    case "X":
+        endProgram()
+    default:
+        print(Description.inputError)
+        getMenu()
+    }
+}
+
+private func addStudent(_: String) {
+    
+}
+
+private func removeStudent(_: String) {
+    
+}
+
+private func addGrade(_: String) {
+    
+}
+
+private func removeGrade(_: String) {
+    
+}
+
+private func getGrade(_: String) {
+    
+}
+
+private func endProgram() {
+    exit(0)
+}
+
+
+getMenu()
