46cd20afc26c019508cbcce5f06f64c91ec03c4c
haneulKimaa <khnpoq@naver.com>
2022-11-18 19:32:17 +0900

    [#1] 성적 enum 추가

---
+++ MyCreditManager/MyCreditManager/Common/Data/Grade.swift
@@ -0,0 +1,20 @@
+//
+//  Grade.swift
+//  MyCreditManager
+//
+//  Created by 김하늘 on 2022/11/18.
+//
+
+import Foundation
+
+enum Grade: Double {
+    case APlus = 4.5
+    case A = 4.0
+    case BPlus = 3.5
+    case B = 3.0
+    case CPlus = 2.5
+    case C = 2.0
+    case DPlus = 1.5
+    case D = 1.0
+    case F = 0.0
+}
